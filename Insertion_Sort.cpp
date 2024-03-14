d2949aef6dd418c0b4803b62533c47ec78de2bce
a <163398502+praccheat@users.noreply.github.com>
2024-03-15 00:58:56 +0530

    Update Insertion_Sort.cpp

--- Insertion_Sort.cpp
+++ Insertion_Sort.cpp
@@ -1,34 +1,25 @@
-#include<IOSTREAM>                                               
+#include<iostream>                                               
 #include<conio.h>
 using namespace std;
-int main()
-{
-  
+int main(){
 	int a[10], i, j,temp,s;
 	cout<<"Enter the array size : ";
 	cin>>s;
-	cout<<"Enter Array Elements : ";
-	for(i=0; i<s; i++)
-	{
+	cout<<"Enter Array Elements : \n";
+	for(i=0; i<s; i++){
 		cin>>a[i];
 	}
-
-   for(i=1;i<s;i++)
-   {
-    temp=a[i];
-    j=i-1;
-     while((temp<a[j])&&(j>=0))
-    {
-     a[j+1]=a[j];
-     j=j-1;
-     }
-     a[j+1]=temp;
-     }
-     for(i=0;i<s;i++)
-     {
-      cout<<a[i]<<"\t"<<"element is sorted using insertion sort\n";
-      }
-      return 0;
-    
+    for(i=1;i<s;i++){
+        temp=a[i];
+        j=i-1;
+        while((temp<a[j])&&(j>=0)){
+            a[j+1]=a[j];
+            j=j-1;
+        }
+        a[j+1]=temp;
+    }
+    for(i=0;i<s;i++){
+        cout<<a[i]<<"\t"<<"element is sorted using insertion sort\n";
+    }
+    return 0;
 }
-
