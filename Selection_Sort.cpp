2699e01ec3a747dfa891f5fc06af63275a80b1a7
a <163398502+praccheat@users.noreply.github.com>
2024-03-15 00:59:42 +0530

    Update Selection_Sort.cpp

--- Selection_Sort.cpp
+++ Selection_Sort.cpp
@@ -1,36 +1,25 @@
-#include<IOSTREAM>
+#include<iostream>
 #include<conio.h>
 using namespace std;
-int main()
-{
-	
-//clrscr();
-int a[50]; int j; int size; int t;
-cout<<"enter the size : ";
-cout<<"\n";
-cin>>size;
-cout<<"enter elements : ";
-cout<<"\n";
-for(int i=0;i<size;i++)
-	{
-	cin>>a[i];
+int main(){
+    int a[50]; int j; int size; int t;
+    cout<<"enter the size : ";
+    cin>>size;
+    cout<<"enter elements : \n";
+    for(int i=0;i<size;i++){
+	    cin>>a[i];
 	}
-for (int i=0;i<size;i++)
-{
-	for(j=i+1;j<size;j++)
-	{
-		if(a[i]>a[j])
-		{
-			t=a[j];
-			a[j]=a[i];
-			a[i]=t;
-		}
-	}
-}
-for(int i=0;i<size;i++)
-{
-	cout<<a[i]<<"\t";
-}
-return 0;
-//getch();                              
+    for (int i=0;i<size;i++){
+	    for(j=i+1;j<size;j++){
+		    if(a[i]>a[j]){
+			    t=a[j];
+			    a[j]=a[i];
+			    a[i]=t;
+		    }
+	    }
+    }
+    for(int i=0;i<size;i++){
+	    cout<<a[i]<<"\t";
+    }
+    return 0;                           
 }
