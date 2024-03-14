c3c78cc179c9b4469af592e855bbbe1ce231168e
a <163398502+praccheat@users.noreply.github.com>
2024-03-15 00:56:44 +0530

    Update Linear_Search.cpp

--- Linear_Search.cpp
+++ Linear_Search.cpp
@@ -1,33 +1,24 @@
-#include<IOSTREAM>
+#include<iostream>
 #include<conio.h>
 using namespace std;
-int main()
-{
-  
-//clrscr();
-int array[5];
-int data;
-cout<<"enter the array\n";
-for(int k=0;k<5;k++)
-{
-  cout<<(k+1)<<" value:";
-  cin>>array[k];
-}
-cout<<"enter the data to search: ";
-cin>>data;
-for(int i=0;i<5;i++)
-{
- if(array[i] == data)
-   {
-    cout<<"data found at position: "<<i+1<<"\n";
-   }
-   if(i == 4){
-      return 0;
+int main() {
+    int array[5];
+    int data;
+    bool found = false;
+    cout << "Enter the array:\n";
+    for (int k = 0; k < 5; k++) {
+        cin >> array[k];
     }
+    cout << "Enter the data to search: ";
+    cin >> data;
+    for (int i = 0; i < 5; i++) {
+        if (array[i] == data) {
+            cout << "Data found at position " << i + 1 << endl;
+            found = true;
+        }
+    }
+    if (found==false) {
+        cout << "Data not found in the array." << endl;
+    }
+    return 0;
 }
-
-cout<<"data not found";
-return 0;
-//getch();
-}
-
